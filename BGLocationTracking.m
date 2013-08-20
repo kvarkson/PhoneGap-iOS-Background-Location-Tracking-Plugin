57f98fb9849cd56da490827aeb35b774d2f89043
Stas Gorodnichenko <sgorodnichenko@itginger.com>
2013-08-20 18:13:27 +0300

    plugin files refactored

--- BGLocationTracking.m
+++ BGLocationTracking.m
@@ -35,26 +35,18 @@
 }
 
 - (void)reInitAndStartLocationManager {
-    NSLog(@"reInitAndStartLocationManager");
     
     [self stopUpdatingLocation:[[CDVInvokedUrlCommand alloc] init]];
     
     self.locationManager = [[CLLocationManager alloc] init];
     locationManager.delegate = self;
-    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+    locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
     [locationManager startUpdatingLocation];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
-    NSLog(@"new location: %@", newLocation);
     
     [self.delegate locationDidUpdate:newLocation];
-    
-    // notify about location update via local notification
-    // UILocalNotification *notif = [[UILocalNotification alloc] init];
-    // notif.alertBody = [NSString stringWithFormat:@"found new location: %@", newLocation];
-    // [[UIApplication sharedApplication] cancelAllLocalNotifications];
-    // [[UIApplication sharedApplication] presentLocalNotificationNow:notif];
     
     // stop location manager
     [self stopUpdatingLocation:[[CDVInvokedUrlCommand alloc] init]];

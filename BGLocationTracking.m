5d702dcd18c376e60b93840e9a9d195e54d360de
kvarkson <kvarkson@gmail.com>
2013-08-22 13:44:08 +0300

    the reinitialize patch applied

--- BGLocationTracking.m
+++ BGLocationTracking.m
@@ -8,18 +8,22 @@
 
 #import "BGLocationTracking.h"
 
+#define LOCATION_MANAGER_LIFETIME_MAX (15 * 60) // in seconds
+
 @interface BGLocationTracking ()
 
 - (void)initAndStartLocationManager;
 
 @property (strong, nonatomic) CLLocationManager *locationManager;
 @property (strong, nonatomic) CDVInvokedUrlCommand *successCB;
+@property (strong, nonatomic) NSDate *locationManagerCreationDate;
 
 @end
 
 
 @implementation BGLocationTracking
 @synthesize locationManager, delegate, successCB;
+@synthesize locationManagerCreationDate;
 
 - (void)startUpdatingLocation:(CDVInvokedUrlCommand *)command {
     [self initAndStartLocationManager];
@@ -41,6 +45,7 @@
     [self stopUpdatingLocation:[[CDVInvokedUrlCommand alloc] init]];
     
     self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManagerCreationDate = [NSDate date];
     locationManager.delegate = self;
     locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
     locationManager.distanceFilter = 10.0;
@@ -53,19 +58,28 @@
     [self.delegate locationDidUpdate:newLocation];
     
     [self callSuccessJSCalback:newLocation.coordinate.latitude :newLocation.coordinate.longitude];
+    //@TODELETE THIS BLOCK
+    UILocalNotification *notif = [[UILocalNotification alloc] init];
+    notif.alertBody = [NSString stringWithFormat:@"found new location: %@", newLocation];
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+    [[UIApplication sharedApplication] presentLocalNotificationNow:notif];
     
+    // if location manager is very old, need to re-init
+    NSDate *currentDate = [NSDate date];
+    if ([currentDate timeIntervalSinceDate:self.locationManagerCreationDate] >= LOCATION_MANAGER_LIFETIME_MAX) {
+        [self initAndStartLocationManager];
+    }
 }
 
 - (void)callSuccessJSCalback:(double)lat :(double)lon {
     [self.webView stringByEvaluatingJavaScriptFromString:
-        [NSString stringWithFormat:@"%@({ coords: { latitude: %f, longitude: %f}});", self.successCB, lat, lon ]];
+     [NSString stringWithFormat:@"%@({ coords: { latitude: %f, longitude: %f}});", self.successCB, lat, lon ]];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
     
     [self.delegate locationDidFailWithError:error];
     NSLog(@"%@", error);
-    [self stopUpdatingLocation:[[CDVInvokedUrlCommand alloc] init]];
     
     [self initAndStartLocationManager];
 }
